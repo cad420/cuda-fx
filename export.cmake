b9209d46cda1c2e233526258c0cd3bae3e0c419d
koishi <qwq@koishi.top>
2019-10-05 11:06:41 +0800

    lnk cuda

--- export.cmake
+++ export.cmake
@@ -1,5 +1,6 @@
 add_library(cudafx src/dummy.cc)
 
+target_link_libraries(cudafx cuda)
 target_include_directories(cudafx INTERFACE
     ${CMAKE_CURRENT_SOURCE_DIR}/include
     ${CMAKE_CUDA_TOOLKIT_INCLUDE_DIRECTORIES}	
