d2bdbd97d382cf0d2ea75c40d0b17a91e64781a4
koishi <qwq@koishi.top>
2019-10-09 11:41:44 +0800

    if driver

--- export.cmake
+++ export.cmake
@@ -1,6 +1,6 @@
 add_library(cudafx src/dummy.cc)
 
-target_link_libraries(cudafx cuda)
+# target_link_libraries(cudafx ${CUDA_CUDA_LIBRARY})  # if need driver lib
 target_include_directories(cudafx INTERFACE
     ${CMAKE_CURRENT_SOURCE_DIR}/include
     ${CMAKE_CUDA_TOOLKIT_INCLUDE_DIRECTORIES}	
